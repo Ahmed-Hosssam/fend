162dc25c818e0e908dce452838a74f16f2295fc4
Ahmed hossam <toxin435@gmail.com>
2021-03-28 02:25:01 +0200

    Modifications to ReadMe.md and fixed some bugs in the active section app.js code

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -1,13 +1,13 @@
 # Landing Page Project
 
+### This is the first project for Udacity's web development professional course.
+
+The Project has starter code. Lots of my code is Javascript code in js/app.js file and some modifications are made in
+index.html file.
+Starter code taken from the course repository.
 ## Table of Contents
-
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+* [Features made](#features made)
+##Features made:
+- Navigation is built dynamically as an unordered list
+- The section being viewed is highlighted so that its clear which section is currently being viewed
+- clicking on any item in the nav-bar scrolls down to that specific section.
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,41 +1,41 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-var sections = document.querySelectorAll('Section');
-var navItems = document.querySelectorAll('.nav_item');
+ *
+ */
+const sections = document.querySelectorAll('section');
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
-function  getIds (){
+
+function getIds() {
     var Ids = [];
-    sections.forEach(function (section){
+    sections.forEach(function (section) {
         Ids.push(section.id);
     });
     return Ids;
 }
 
-function  getData (){
+function getData() {
     var data = [];
-    sections.forEach(function (section){
+    sections.forEach(function (section) {
         data.push(section.dataset.nav);
     });
     return data;
@@ -44,23 +44,24 @@ function  getData (){
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
-sections.forEach(function (section){
+sections.forEach(function (section) {
     var ul = document.getElementById("navbar__list");
     var li = document.createElement("li");
     var a = document.createElement("a");
     a.appendChild(document.createTextNode(section.dataset.nav));
-    a.setAttribute("id","nav_"+section.id);
-    a.setAttribute("class","nav_item");
-    a.setAttribute("href","");
-    a.setAttribute("onclick","return false;");
+    a.setAttribute("id", "nav_" + section.id);
+    a.setAttribute("class", "nav_item");
+    a.setAttribute("href", "");
+    a.setAttribute("onclick", "return false;");
     li.appendChild(a);
     ul.appendChild(li);
 });
-navItems.forEach(function (item,index,array){
+const navItems = document.querySelectorAll('.nav_item');
+navItems.forEach(function (item, index, array) {
     var sectionID = "#" + array[index].id.slice(4);
     var section = document.querySelector(sectionID);
     array[index].addEventListener('click', function () {
@@ -81,8 +82,8 @@ navItems.forEach(function (item,index,array){
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
 // Build menu 
 
@@ -90,20 +91,22 @@ navItems.forEach(function (item,index,array){
 
 // Set sections as active
 
-function set(){
-    var section=document.querySelectorAll(".nav_item");
-    for (let i=0; i < sections.length; i++){
-        if (InView(sections[i])){
+function set() {
+    var section = document.querySelectorAll(".nav_item");
+    for (let i = 0; i < sections.length; i++) {
+        if (InView(sections[i])) {
             section[i].classList.add("active-section");
-        }else{
+        } else {
             section[i].classList.remove("active-section");
         }
     }
 }
-document.addEventListener('scroll', function(){
+
+document.addEventListener('scroll', function () {
     set();
 });
-function InView (element) {
+
+function InView(element) {
     const bound = element.getBoundingClientRect();
     return (
         bound.top >= 0 &&
